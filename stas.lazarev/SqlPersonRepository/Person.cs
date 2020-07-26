7be4f21d1cfec0e739786be30862b34434d1eeff
stas-lazarev <staslazlegolas@gmail.com>
2020-07-26 10:40:58 +0300

    [1.2]Add repository proj

---
+++ stas.lazarev/SqlPersonRepository/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SqlPersonRepository
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Sex { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public byte? Age { get; set; }
+        public bool? IsAdult { get; set; }
+        public decimal? Balance { get; set; }
+    }
+}
