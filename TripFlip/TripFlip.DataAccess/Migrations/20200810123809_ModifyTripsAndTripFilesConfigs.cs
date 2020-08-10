9af6ca3752d0698ef696aedc5631190605b0ea57
gingerbread-monster <staslazlegolas@gmail.com>
2020-08-10 15:39:09 +0300

    [2.1] add migration

---
+++ TripFlip/TripFlip.DataAccess/Migrations/20200810123809_ModifyTripsAndTripFilesConfigs.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TripFlip.DataAccess.Migrations
+{
+    public partial class ModifyTripsAndTripFilesConfigs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_TripFiles_TripId",
+                table: "TripFiles",
+                column: "TripId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TripFiles_Trips_TripId",
+                table: "TripFiles",
+                column: "TripId",
+                principalTable: "Trips",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TripFiles_Trips_TripId",
+                table: "TripFiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TripFiles_TripId",
+                table: "TripFiles");
+        }
+    }
+}
