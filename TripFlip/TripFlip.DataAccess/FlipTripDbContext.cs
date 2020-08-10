8ac0d08228629e986d82f697485fa8a56bb837eb
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-08-10 11:24:04 +0300

    [2.1] Add classes DbContext, Constants.

---
+++ TripFlip/TripFlip.DataAccess/FlipTripDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TripFlip.DataAccess.Entities;
+
+namespace TripFlip.DataAccess
+{
+    public class FlipTripDbContext : DbContext
+    {
+        public DbSet<ItemEntity> Items { get; set; }
+
+        public DbSet<ItemListEntity> ItemLists { get; set; }
+
+        public DbSet<RouteEntity> Routes { get; set; }
+
+        public DbSet<RoutePointEntity> RoutePoints { get; set; }
+
+        public DbSet<TaskEntity> Tasks { get; set; }
+
+        public DbSet<TaskListEntity> TaskLists { get; set; }
+
+        public DbSet<TripEntity> Trips { get; set; }
+
+        public DbSet<TripFileEntity> TripFiles { get; set; }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Constants.MsSqlLocalDbConnection);
+        }
+    }
+}
