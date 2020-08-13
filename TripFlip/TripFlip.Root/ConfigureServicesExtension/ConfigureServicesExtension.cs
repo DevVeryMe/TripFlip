7876de00ebd528b6b9e3ffb075143212f5c691f0
gingerbread-monster <staslazlegolas@gmail.com>
2020-08-14 01:23:08 +0300

    [TN202B-86] configure FlipTripDbContext to use connection string from appsettings.json

--- TripFlip/TripFlip.Root/ConfigureServicesExtension/ConfigureServicesExtension.cs
+++ TripFlip/TripFlip.Root/ConfigureServicesExtension/ConfigureServicesExtension.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using TripFlip.Services;
 using TripFlip.Services.Interfaces;
+using TripFlip.DataAccess;
 
 namespace TripFlip.Root.ConfigureServicesExtension
 {
     public static class ConfigureServicesExtension
     {
-        public static void ConfigureServices(this IServiceCollection services)
+        public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddTransient<IGreetingService, GreetingService>();
+
+            string defaultConnectionStringName = "DefaultConnection";
+            services.AddDbContext<FlipTripDbContext>(options =>
+                options.UseSqlServer(
+                    ConfigurationExtensions.GetConnectionString(
+                        configuration, 
+                        defaultConnectionStringName
+                    )
+                )
+            );
         }
     }
 }
