c8ce2aca980207866ce15df421d18bb3108e174a
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-08-13 19:26:37 +0300

    [TN202B-13] Implement first version of exception handler.

---
+++ TripFlip/TripFlip.WebApi/ExceptionHandlers/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using NLog;
+using TripFlip.WebApi.ExceptionFilters.Models;
+
+namespace TripFlip.WebApi.ExceptionFilters
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if (contextFeature != null)
+                    {
+                        logger.Error(contextFeature);
+
+                        var errorDetails = new ErrorDetails
+                        {
+                            StatusCode = (int) HttpStatusCode.InternalServerError,
+                            Message = "Internal server error."
+                        };
+
+                        if (environment.IsDevelopment())
+                        {
+                            errorDetails.DroppedException = contextFeature.Error;
+                        }
+
+                        await context.Response.WriteAsync(errorDetails.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
--- TripFlip/TripFlip.WebApi/Startup.cs
+++ TripFlip/TripFlip.WebApi/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TripFlip.WebApi.ExceptionFilters;
 
 namespace TripFlip.WebApi
 {
@@ -28,6 +29,8 @@ namespace TripFlip.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            app.ConfigureExceptionHandler(env);
 
             app.UseRouting();
 
