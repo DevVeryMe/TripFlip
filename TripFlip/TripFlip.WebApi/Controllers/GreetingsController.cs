9be3dab5c671e2e193c28399a1fadd2fc68f2eff
gingerbread-monster <staslazlegolas@gmail.com>
2020-08-14 15:45:48 +0300

    [TN202B-81] refactor temp controller

---
+++ TripFlip/TripFlip.WebApi/Controllers/GreetingsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TripFlip.Services.Interfaces;
+
+namespace TripFlip.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingsController : ControllerBase
+    {
+        private readonly IGreetingService _greetingService;
+
+        public GreetingsController(IGreetingService greetingService)
+        {
+            _greetingService = greetingService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string greetingString = _greetingService.GetGreeting();
+            return Ok(greetingString);
+        }
+    }
+}
