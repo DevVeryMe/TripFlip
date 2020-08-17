ce16a8e44be61a70760ff46b85c1420a681b7421
Andriy Veremiy <andrewveremiyy@gmail.com>
2020-08-17 16:21:26 +0300

    Created TaskList ViewModels.

---
+++ TripFlip/TripFlip.ViewModels/TaskListViewModels/FullTaskListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TripFlip.ViewModels.TaskListViewModels
+{
+    public class FullTaskListViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int RouteId { get; set; }
+    }
+}
