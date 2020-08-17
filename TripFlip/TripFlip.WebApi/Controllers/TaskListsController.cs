9dfa491aa458438e9e69cd88047e4e49cd2899d1
Andriy Veremiy <andrewveremiyy@gmail.com>
2020-08-17 16:25:15 +0300

    Created TaskLists Controller.

---
+++ TripFlip/TripFlip.WebApi/Controllers/TaskListsController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TripFlip.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskListsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public TaskListsController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+    }
+}
