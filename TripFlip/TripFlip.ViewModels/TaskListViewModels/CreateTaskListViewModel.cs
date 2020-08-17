ada7aa7aa602fe4fe56e9f74bd94ff45d7f6a93e
Andriy Veremiy <andrewveremiyy@gmail.com>
2020-08-18 02:03:18 +0300

    Added validation to TaskList ViewModels.

--- TripFlip/TripFlip.ViewModels/TaskListViewModels/CreateTaskListViewModel.cs
+++ TripFlip/TripFlip.ViewModels/TaskListViewModels/CreateTaskListViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TripFlip.ViewModels.TaskListViewModels
 {
     public class CreateTaskListViewModel
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = ErrorConstants.EmptyTitleError)]
+        [StringLength(100, ErrorMessage = ErrorConstants.TitleLengthError)]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = ErrorConstants.RequiredIdError)]
+        [Range(1, int.MaxValue, ErrorMessage = ErrorConstants.IdLessThanOneError)]
         public int RouteId { get; set; }
     }
 }
--- TripFlip/TripFlip.ViewModels/TaskListViewModels/UpdateTaskListViewModel.cs
+++ TripFlip/TripFlip.ViewModels/TaskListViewModels/UpdateTaskListViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TripFlip.ViewModels.TaskListViewModels
 {
     public class UpdateTaskListViewModel
     {
+        [Required(ErrorMessage = ErrorConstants.RequiredIdError)]
+        [Range(1, int.MaxValue, ErrorMessage = ErrorConstants.IdLessThanOneError)]
         public int Id { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = ErrorConstants.EmptyTitleError)]
+        [StringLength(100, ErrorMessage = ErrorConstants.TitleLengthError)]
         public string Title { get; set; }
     }
 }
