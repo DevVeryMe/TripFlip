f991c4a51e19b8914fd2ec972082f733524fbd46
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-08-17 19:27:37 +0300

    [TN202B-110] Implement IItemService and ItemDto.

---
+++ TripFlip/TripFlip.Services.DTO/ItemDtos/ItemDto.cs
@@ -0,0 +1,19 @@
+﻿using TripFlip.Services.DTO.Enums;
+
+namespace TripFlip.Services.DTO.ItemDtos
+{
+    public class ItemDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Comment { get; set; }
+
+        public string Quantity { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public int ItemListId { get; set; }
+    }
+}
---
+++ TripFlip/TripFlip.Services.Interfaces/IItemService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TripFlip.Services.DTO.ItemDtos;
+
+namespace TripFlip.Services.Interfaces
+{
+    public interface IItemService
+    {
+        /// <summary>
+        /// Gets all items of certain item list.
+        /// </summary>
+        Task<IEnumerable<ItemDto>> GetAllAsync(int itemListId);
+
+        /// <summary>
+        /// Gets item by id.
+        /// </summary>
+        /// <param name="id">item id</param>
+        Task<ItemDto> GetAsync(int id);
+
+        /// <summary>
+        /// Creates new item.
+        /// </summary>
+        /// <param name="itemDto">item data</param>
+        /// <returns>created item</returns>
+        Task<ItemDto> CreateAsync(ItemDto itemDto);
+
+        /// <summary>
+        /// Updates existing item.
+        /// </summary>
+        /// <param name="itemDto">new item data</param>
+        /// <returns>updated item</returns>
+        Task<ItemDto> UpdateAsync(ItemDto itemDto);
+
+        /// <summary>
+        /// Deletes item.
+        /// </summary>
+        /// <param name="id">item to delete id</param>
+        Task DeleteAsync(int id);
+    }
+}
