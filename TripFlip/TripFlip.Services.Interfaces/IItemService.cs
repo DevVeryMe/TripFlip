2a6a1f7cce4ffd68ca2f26d616df62a5cbede26d
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-08-18 16:14:48 +0300

    [TN202B-110] Add check for existing item list, remove methods, which are not related to this task.

--- TripFlip/TripFlip.Services.Interfaces/IItemService.cs
+++ TripFlip/TripFlip.Services.Interfaces/IItemService.cs
@@ -7,34 +7,10 @@ namespace TripFlip.Services.Interfaces
     public interface IItemService
     {
         /// <summary>
-        /// Gets all items of certain item list.
-        /// </summary>
-        Task<IEnumerable<ItemDto>> GetAllAsync(int itemListId);
-
-        /// <summary>
-        /// Gets item by id.
-        /// </summary>
-        /// <param name="id">item id</param>
-        Task<ItemDto> GetAsync(int id);
-
-        /// <summary>
         /// Creates new item.
         /// </summary>
         /// <param name="createItemDto">item data</param>
         /// <returns>created item</returns>
         Task<ItemDto> CreateAsync(CreateItemDto createItemDto);
-
-        /// <summary>
-        /// Updates existing item.
-        /// </summary>
-        /// <param name="createItemDto">new item data</param>
-        /// <returns>updated item</returns>
-        Task<ItemDto> UpdateAsync(CreateItemDto createItemDto);
-
-        /// <summary>
-        /// Deletes item.
-        /// </summary>
-        /// <param name="id">item to delete id</param>
-        Task DeleteAsync(int id);
     }
 }
