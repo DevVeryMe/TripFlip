2ad316c0bab8b8b669aebf71f7820ebaf779f0e6
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-08-18 14:26:15 +0300

    [TN202B-93] Change single to single or default.

--- TripFlip/TripFlip.Services/TripService.cs
+++ TripFlip/TripFlip.Services/TripService.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using TripFlip.DataAccess;
 using TripFlip.Services.DTO;
@@ -31,7 +32,7 @@ namespace TripFlip.Services
         }
         public async Task<TripDto> GetAsync(int id)
         {
-            var trip = await _flipTripDbContext.Trips.AsNoTracking().SingleAsync(t => t.Id == id);
+            var trip = await _flipTripDbContext.Trips.AsNoTracking().SingleOrDefaultAsync(t => t.Id == id);
 
             if (trip is null)
             {
