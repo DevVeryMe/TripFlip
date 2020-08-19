45e63ead1c0cb2e0a9fdb873c2a829b7138d5f86
Andriy Veremiy <andrewveremiyy@gmail.com>
2020-08-19 16:04:31 +0300

    Added enum to enum mapping profile.

---
+++ TripFlip/TripFlip.Root/MappingProfiles/EnumToEnum.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace TripFlip.Root.MappingProfiles
+{
+    public class EnumToEnum : Profile
+    {
+        public EnumToEnum()
+        {
+            CreateMap<Services.DTO.Enums.TaskPriorityLevel, Domain.Entities.Enums.TaskPriorityLevel>();
+        }
+    }
+}
