b00e2de7d304725eaa2de6f949065b353b9191cc
Andriy Veremiy <andrewveremiyy@gmail.com>
2020-08-20 09:59:20 +0000

    TaskListService.cs edited online with Bitbucket

--- TripFlip/TripFlip.Services/TaskListService.cs
+++ TripFlip/TripFlip.Services/TaskListService.cs
@@ -42,7 +42,7 @@ namespace TripFlip.Services
         }
 
         /// <summary>
-        /// Validates wether route with specified id exists or not.
+        /// Validates whether route with specified id exists or not.
         /// Throws an exception if route with specified id doesn't exist.
         /// </summary>
         /// <param name="routeId">Route id.</param>
