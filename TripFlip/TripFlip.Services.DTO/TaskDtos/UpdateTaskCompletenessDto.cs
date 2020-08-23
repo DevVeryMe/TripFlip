0ac5d2f3aae660c1502e55fc4ab70ab9973fb848
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-08-23 16:13:58 +0300

    [TN202B-139] Implement separate endpoint for changing task completeness.

---
+++ TripFlip/TripFlip.Services.DTO/TaskDtos/UpdateTaskCompletenessDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TripFlip.Services.DTO.TaskDtos
+{
+    public class UpdateTaskCompletenessDto
+    {
+        public int Id { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
