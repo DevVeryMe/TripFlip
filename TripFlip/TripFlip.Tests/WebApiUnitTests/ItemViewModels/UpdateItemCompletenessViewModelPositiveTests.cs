966954c37ba93462f0d7d3c96a287c546a3e3c79
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-08-27 18:38:16 +0300

    [TN202B-160] Add positive tests for UpdateItemCompletenessViewModel.

---
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/ItemViewModels/UpdateItemCompletenessViewModelPositiveTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TripFlip.ViewModels.ItemViewModels;
+
+namespace WebApiUnitTests.ItemViewModels
+{
+    [TestClass]
+    public class UpdateItemCompletenessViewModelPositiveTests
+    {
+        [DataTestMethod]
+        public void Create_UpdateItemCompletenessViewModel_Given_Valid_values_Should_be_successful()
+        {
+            var updateItemCompletenessViewModel = GetUpdateItemCompletenessViewModel(1, true);
+
+            var result = Validator.TryValidateObject(updateItemCompletenessViewModel,
+                new ValidationContext(updateItemCompletenessViewModel),
+                null,
+                true);
+
+            Assert.IsTrue(result);
+        }
+
+        private static UpdateItemCompletenessViewModel GetUpdateItemCompletenessViewModel(int id, bool isCompleted)
+        {
+            return new UpdateItemCompletenessViewModel()
+            {
+                Id = id,
+                IsCompleted = isCompleted
+            };
+        }
+    }
+}
