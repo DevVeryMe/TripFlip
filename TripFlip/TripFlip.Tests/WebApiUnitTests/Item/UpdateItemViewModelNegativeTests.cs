8924b11225e8d3dabc934882721c7a0f0781a87b
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-08-27 19:59:40 +0300

    [TN202B-160] Changed long hardcoded strings to filling them from string constructor.

--- TripFlip/TripFlip.Tests/WebApiUnitTests/Item/UpdateItemViewModelNegativeTests.cs
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/Item/UpdateItemViewModelNegativeTests.cs
@@ -38,16 +38,7 @@ namespace WebApiUnitTests.Item
         [DataTestMethod]
         public void Create_UpdateItemViewModel_Given_Not_valid_Comment_too_long_Validation_should_be_failed()
         {
-            var comment = "The European languages are members of the same family. Their separate " +
-                          "existence is a myth. For science, music, sport, etc, Europe uses the same" +
-                          " vocabulary. The languages only differ in their grammar, their pronunciation" +
-                          " and their most common words. Everyone realizes why a new common language would" +
-                          " be desirable: one could refuse to pay expensive translators. To achieve this," +
-                          " it would be necessary to have uniform grammar, pronunciation and more common words." +
-                          " If several languages coalesce, the grammar of the resulting language is more" +
-                          " simple and regular than that of the individual languages. The new common language" +
-                          " will be more simple and regular than the existing European languages. It " +
-                          "will be as simple as Occidental; in fact, it will be Occidental.";
+            var comment = new string('*', 251);
 
             var createItemViewModel = GetUpdateItemViewModel(1, "Tent", comment, null, true);
 
@@ -62,8 +53,7 @@ namespace WebApiUnitTests.Item
         [DataTestMethod]
         public void Create_UpdateItemViewModel_Given_Not_valid_Quantity_too_long_Validation_should_be_failed()
         {
-            var quantity = "The European languages are members of the same family. Their separate " +
-                          "existence is a myth.";
+            var quantity = new string('*', 51);
 
             var createItemViewModel = GetUpdateItemViewModel(1, "Tent", null, quantity, true);
 
@@ -95,10 +85,7 @@ namespace WebApiUnitTests.Item
             {
                 "Test case 3: Create_UpdateItemViewModel_Given_Not_valid_Title_too_long_length_" +
                 "Validation_should_be_failed",
-
-                "The European languages are members of the same family. Their separate existence is a myth. " +
-                "For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in" +
-                " their grammar, their pronunciation and their most common words."
+                new string('*', 101)
             };
         }
 
