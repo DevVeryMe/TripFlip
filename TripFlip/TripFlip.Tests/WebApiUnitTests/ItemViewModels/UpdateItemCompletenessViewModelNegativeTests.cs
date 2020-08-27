8c50f96e2d9d433014503b8905a96529d959a9ea
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-08-27 18:43:22 +0300

    [TN202B-160] Add negative tests for UpdateItemCompletenessViewModel.

---
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/ItemViewModels/UpdateItemCompletenessViewModelNegativeTests.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TripFlip.ViewModels.ItemViewModels;
+
+namespace WebApiUnitTests.ItemViewModels
+{
+    [TestClass]
+    public class UpdateItemCompletenessViewModelNegativeTests
+    {
+        [DataTestMethod]
+        public void Create_UpdateItemCompletenessViewModel_Given_Not_valid_Id_Should_be_failed()
+        {
+            var updateItemCompletenessViewModel = GetUpdateItemCompletenessViewModel(-1, true);
+
+            var result = Validator.TryValidateObject(updateItemCompletenessViewModel,
+                new ValidationContext(updateItemCompletenessViewModel),
+                null,
+                true);
+
+            Assert.IsFalse(result);
+        }
+
+        private static UpdateItemCompletenessViewModel GetUpdateItemCompletenessViewModel(int id, bool isCompleted)
+        {
+            return new UpdateItemCompletenessViewModel()
+            {
+                Id = id,
+                IsCompleted = isCompleted
+            };
+        }
+    }
+}
