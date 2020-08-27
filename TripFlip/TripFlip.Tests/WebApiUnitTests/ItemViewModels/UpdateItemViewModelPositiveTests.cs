9bf126e32eae36c1eb690d28412e767f9bcc45c5
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-08-27 18:10:36 +0300

    [TN202B-160] Add UpdateItemViewModel positive tests.

---
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/ItemViewModels/UpdateItemViewModelPositiveTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TripFlip.ViewModels.ItemViewModels;
+
+namespace WebApiUnitTests.ItemViewModels
+{
+    [TestClass]
+    public class UpdateItemViewModelPositiveTests
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetUpdateItemViewModelData), DynamicDataSourceType.Method)]
+        public void TestItemViewModelValidation(string displayName,
+            UpdateItemViewModel updateItemViewModel)
+        {
+            var result = Validator.TryValidateObject(updateItemViewModel,
+                new ValidationContext(updateItemViewModel),
+                null,
+                true);
+
+            Assert.IsTrue(result);
+        }
+
+        private static IEnumerable<object[]> GetUpdateItemViewModelData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Create_UpdateItemViewModel_Given_Valid_Not_null_values_Should_be_successful",
+                GetUpdateItemViewModel(1, "Tent", null, null,true)
+            };
+
+            yield return new object[]
+            {
+                "Test case 1: Create_UpdateItemViewModel_Given_Valid_With_null_values_where_possible_Should_be_successful",
+                GetUpdateItemViewModel(1, "Tent", "The most needed item.", "At least 1.",true)
+            };
+        }
+
+        private static UpdateItemViewModel GetUpdateItemViewModel(int id, string title, 
+            string comment, string quantity, bool isCompleted)
+        {
+            return new UpdateItemViewModel()
+            {
+                Id = id,
+                Title = title,
+                Comment = comment,
+                Quantity = quantity,
+                IsCompleted = isCompleted
+            };
+        }
+    }
+}
