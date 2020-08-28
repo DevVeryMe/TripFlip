6696d11497d3da756889ccb561b428b0b3c67653
gingerbread-monster <staslazlegolas@gmail.com>
2020-08-28 12:37:38 +0300

    [TN202B-159] move method that returns test cases back into each test class

--- TripFlip/TripFlip.Tests/WebApiUnitTests/ItemList/CreateItemListViewModel_Negative_Tests.cs
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/ItemList/CreateItemListViewModel_Negative_Tests.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TripFlip.ViewModels.ItemListViewModels;
 
@@ -8,7 +9,7 @@ namespace WebApiUnitTests.ItemList
     public class CreateItemListViewModel_Negative_Tests
     {
         [DataTestMethod]
-        [DynamicData(nameof(Helpers.Get_Invalid_ItemList_Title), typeof(Helpers), DynamicDataSourceType.Method)]
+        [DynamicData(nameof(Get_Invalid_ItemList_Title), DynamicDataSourceType.Method)]
         public void Test_ItemList_Title_Validation(
             string testCaseDisplayName,
             string title,
@@ -51,6 +52,34 @@ namespace WebApiUnitTests.ItemList
 
             // Assert
             Assert.IsFalse(result);
+        }
+
+        static IEnumerable<object[]> Get_Invalid_ItemList_Title()
+        {
+            yield return new object[]
+            {
+                "Test case 1 : Test_ItemList_Title_Validation was given invalid title equals null." +
+                " Validation should be failed.",
+                null,
+                1
+            };
+
+            yield return new object[]
+            {
+                "Test case 2 : Test_ItemList_Title_Validation was given invalid empty Title string." +
+                " Validation should be failed.",
+                string.Empty,
+                1
+            };
+
+            yield return new object[]
+            {
+                "Test case 3 : Test_ItemList_Title_Validation was given invalid title string" +
+                " with the length of 101 (which exceeds allowed string length of 100 characters)." +
+                " Validation should be failed.",
+                new string('*', 101),
+                1
+            };
         }
     }
 }
