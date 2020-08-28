8c2a1e3098d380dc750f2d5f6e93a13f0878b2c3
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-08-28 19:49:13 +0300

    [TN202B-162] Implement tests for CreateTaskListViewModel.

---
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/CreateTaskListViewModelTests/CreateTaskViewModelNegativeTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TripFlip.ViewModels.TaskListViewModels;
+
+namespace WebApiUnitTests.CreateTaskListViewModelTests
+{
+    [TestClass]
+    public class CreateTaskViewModelNegativeTests
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestTitleData), DynamicDataSourceType.Method)]
+        public void TestTaskListTitleValidation(string displayName, string title)
+        {
+            // Arrange
+            var createTaskListViewModel = GetCreateTaskListViewModel(title: title);
+
+            // Act
+            var result = Validator.TryValidateObject(createTaskListViewModel,
+                new ValidationContext(createTaskListViewModel),
+                null,
+                true);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Create_CreateItemViewModel_Given_Not_valid_ItemListId_Validation_should_be_failed()
+        {
+            // Arrange
+            var createTaskListViewModel = GetCreateTaskListViewModel(routeId: -1);
+
+            // Act
+            var result = Validator.TryValidateObject(createTaskListViewModel,
+                new ValidationContext(createTaskListViewModel),
+                null,
+                true);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        private static IEnumerable<object[]> GetTestTitleData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Create_CreateTaskListViewModel_Given_Not_valid_Title_equals_null_" +
+                "Validation_should_be_failed",
+                null
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: Create_CreateTaskListViewModel_Given_Not_valid_Title_equals_empty_string_" +
+                "Validation_should_be_failed",
+                string.Empty
+            };
+
+            yield return new object[]
+            {
+                "Test case 3: Create_CreateTaskListViewModel_Given_Not_valid_Title_too_long_length_" +
+                "Validation_should_be_failed",
+                new string('*', 101)
+            };
+        }
+
+        private CreateTaskListViewModel GetCreateTaskListViewModel(
+            string title = "Valid value", int routeId = 1)
+        {
+            return new CreateTaskListViewModel()
+            {
+                Title = title,
+                RouteId = routeId
+            };
+        }
+    }
+}
---
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/CreateTaskListViewModelTests/CreateTaskViewModelPositiveTests.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TripFlip.ViewModels.TaskListViewModels;
+
+namespace WebApiUnitTests.CreateTaskListViewModelTests
+{
+    [TestClass]
+    public class CreateTaskViewModelPositiveTests
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestTitleData), DynamicDataSourceType.Method)]
+        public void TestTaskListTitleValidation(string displayName,
+            string title)
+        {
+            // Arrange
+            var createTaskViewViewModel = GetCreateTaskListViewModel(title: title);
+
+            // Act
+            var result = Validator.TryValidateObject(createTaskViewViewModel,
+                new ValidationContext(createTaskViewViewModel),
+                null,
+                true);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestRouteIdData), DynamicDataSourceType.Method)]
+        public void TestTaskListRouteIdValidation(string displayName,
+            int routeId)
+        {
+            // Arrange
+            var createTaskViewViewModel = GetCreateTaskListViewModel(routeId: routeId);
+
+            // Act
+            var result = Validator.TryValidateObject(createTaskViewViewModel,
+                new ValidationContext(createTaskViewViewModel),
+                null,
+                true);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        private static IEnumerable<object[]> GetTestTitleData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Create_CreateTaskListViewModel_Given_Valid_Title_equals_min_value_" +
+                "Validation_should_be_successful",
+                "*"
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: Create_CreateTaskListViewModel_Given_Valid_Title_equals_max_value_" +
+                "Validation_should_be_successful",
+                new string('*', 100)
+            };
+
+            yield return new object[]
+            {
+                "Test case 3: Create_CreateTaskListViewModel_Given_Valid_Title_equals_average_value_" +
+                "Validation_should_be_successful",
+                new string('*', 50)
+            };
+        }
+
+        private static IEnumerable<object[]> GetTestRouteIdData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Create_CreateTaskListViewModel_Given_Valid_RouteId_equals_min_value_" +
+                "Validation_should_be_successful",
+                1
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: Create_CreateTaskListViewModel_Given_Valid_RouteId_equals_average_value_" +
+                "Validation_should_be_successful",
+                2500
+            };
+
+            yield return new object[]
+            {
+                "Test case 3: Create_CreateTaskListViewModel_Given_Valid_RouteId_equals_max_value_" +
+                "Validation_should_be_successful",
+                int.MaxValue
+            };
+        }
+
+
+
+        private CreateTaskListViewModel GetCreateTaskListViewModel(
+            string title = "Valid value", int routeId = 1)
+        {
+            return new CreateTaskListViewModel()
+            {
+                Title = title,
+                RouteId = routeId
+            };
+        }
+    }
+}
