2c43331acd1986decc0628f9508f501fe4aa7f05
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-09-03 19:00:43 +0300

    [TN202B-184] Add UsersController and empty UpdateAsync action.

---
+++ TripFlip/TripFlip.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TripFlip.Services.Interfaces;
+using TripFlip.ViewModels.TripViewModels;
+using TripFlip.ViewModels.UserViewModels;
+
+namespace TripFlip.WebApi.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        private readonly IMapper _mapper;
+
+        public UsersController(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Updates User.
+        /// </summary>
+        /// <param name="updateUserViewModel">New User data with existing User id.</param>
+        /// <returns>User view model that
+        /// represents the updated database entry.</returns>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     PUT /users
+        ///     {
+        ///         "id": 0f8fad5b-d9cb-469f-a165-70867728950e,
+        ///         "email": "sample@gmail.com",
+        ///         "password": "123456",
+        ///     }
+        /// </remarks>
+        [HttpPut]
+        [ProducesResponseType(typeof(UpdateTripViewModel), StatusCodes.Status200OK)]
+        public async Task<IActionResult> UpdateAsync([FromBody] UpdateUserViewModel updateUserViewModel)
+        {
+            return Ok();
+        }
+    }
+}
