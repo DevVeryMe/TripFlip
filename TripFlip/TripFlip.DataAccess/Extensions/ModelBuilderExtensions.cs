70dc52724ab54572c04908b9ef091131589f94af
gingerbread-monster <staslazlegolas@gmail.com>
2020-09-05 21:39:20 +0300

    [TN202B-194] add migration for adding trip role management entities

--- TripFlip/TripFlip.DataAccess/Extensions/ModelBuilderExtensions.cs
+++ TripFlip/TripFlip.DataAccess/Extensions/ModelBuilderExtensions.cs
@@ -116,14 +116,17 @@ namespace TripFlip.DataAccess.Extensions
             modelBuilder.Entity<TripRoleEntity>().HasData(
                 new TripRoleEntity()
                 {
+                    Id = 1,
                     Name = "Admin"
                 },
                 new TripRoleEntity()
                 {
+                    Id = 2,
                     Name = "Editor"
                 },
                 new TripRoleEntity()
                 {
+                    Id = 3,
                     Name = "Guest"
                 });
         }
