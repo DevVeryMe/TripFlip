1fb04bf2532bf3ff3be058b4ec2bdb25c2cbd5fa
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-09-06 19:09:32 +0300

    [TN202B-196] Add GrantSubscriberRoleDto and GrantSubscriberRoleViewModel and needed error constants.

---
+++ TripFlip/TripFlip.Services.DTO/TripSubscribersRolesDtos/GrantSubscriberRoleDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TripFlip.Services.Dto.TripSubscribersRolesDtos
+{
+    public class GrantSubscriberRoleDto
+    {
+        public int TripSubscriberId { get; set; }
+
+        public int RoleId { get; set; }
+    }
+}
---
+++ TripFlip/TripFlip.ViewModels/TripSubscribersRolesViewModels/GrantSubscriberRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TripFlip.ViewModels.TripSubscribersRolesViewModels
+{
+    public class GrantSubscriberRoleViewModel
+    {
+        [Required(ErrorMessage = ErrorConstants.RequiredTripSubscriberIdError)]
+        [Range(1, int.MaxValue, ErrorMessage = ErrorConstants.TripSubscriberIdLessThanOneError)]
+        public int TripSubscriberId { get; set; }
+
+        [Required(ErrorMessage = ErrorConstants.RequiredRoleIdError)]
+        [Range(1, int.MaxValue, ErrorMessage = ErrorConstants.RoleIdLessThanOneError)]
+        public int RoleId { get; set; }
+    }
+}
