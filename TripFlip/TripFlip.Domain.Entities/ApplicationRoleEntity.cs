a1a316b51fb498335d7d6d8dd020b5e4a3cb5931
Andriy Veremiy <andrewveremiyy@gmail.com>
2020-09-07 21:08:15 +0300

    [TN202B-199] Added ApplicationRole and ApplicationUserRole entities.

---
+++ TripFlip/TripFlip.Domain.Entities/ApplicationRoleEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TripFlip.Domain.Entities
+{
+    public class ApplicationRoleEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<ApplicationUserRoleEntity> Users { get; set; }
+    }
+}
