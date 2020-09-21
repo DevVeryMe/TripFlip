22047ac2506e6871a13816100dd4fb495bda53bd
Andriy Veremiy <andrewveremiyy@gmail.com>
2020-09-22 01:37:24 +0300

    [TN202B-241] Implemented Unit tests for GrantApplicationRroles view model.

---
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/GrantApplicationRolesViewModelTests/GrantApplicationRolesViewModelPositiveTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using WebApiUnitTests.Helpers;
+
+namespace WebApiUnitTests.GrantApplicationRolesViewModelTests
+{
+    [TestClass]
+    public class GrantApplicationRolesViewModelPositiveTests
+        : GrantApplicationRolesViewModelTestsBase
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestUserIdData), DynamicDataSourceType.Method)]
+        public void UserId_Validation_Successful(string displayName,
+            Guid userId)
+        {
+            // Arrange
+            var validApplicationRoleIds = new int[] { };
+
+            var grantApplicationRolesViewModel = 
+                BuildGrantApplicationRolesViewModel(validApplicationRoleIds, userId);
+
+            // Act
+            bool modelIsValid = ModelValidator.IsValid(grantApplicationRolesViewModel);
+
+            // Assert
+            Assert.IsTrue(modelIsValid, displayName);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestApplicationRoleIdsData), DynamicDataSourceType.Method)]
+        public void ApplicationRoleIds_Validation_Successful(string displayName,
+            IEnumerable<int> validApplicationRoleIds)
+        {
+            // Arrange
+            var grantApplicationRolesViewModel = 
+                BuildGrantApplicationRolesViewModel(validApplicationRoleIds);
+
+            // Act
+            bool midelIsValid = ModelValidator.IsValid(grantApplicationRolesViewModel);
+
+            // Assert
+            Assert.IsTrue(midelIsValid, displayName);
+        }
+
+        private static IEnumerable<object[]> GetTestUserIdData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Build GrantApplicationRolesViewModel object" +
+                " and set UserId field with MIN allowed valid value.",
+                Guid.Empty
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: Build GrantApplicationRolesViewModel object" +
+                " and set UserId field with average valid value.",
+                Guid.Parse("0f8fad5b-d9cb-469f-a165-70867728950e")
+            };
+        }
+
+        private static IEnumerable<object[]> GetTestApplicationRoleIdsData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Build GrantApplicationRolesViewModel object" +
+                " and make ApplicationRoleIds array is empty.",
+                new int[] { }
+        };
+
+            yield return new object[]
+            {
+                "Test case 2: Build GrantApplicationRolesViewModel object" +
+                " and set ApplicationRoleIds array with average valid values.",
+                new int[] { 1, 2, 3 }
+            };
+        }
+    }
+}
---
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/GrantApplicationRolesViewModelTests/GrantApplicationRolesViewModelTestsBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using TripFlip.ViewModels.UserViewModels;
+
+namespace WebApiUnitTests.GrantApplicationRolesViewModelTests
+{
+    public abstract class GrantApplicationRolesViewModelTestsBase
+    {
+        protected static GrantApplicationRolesViewModel BuildGrantApplicationRolesViewModel(
+            IEnumerable<int> applicationRoleIds,
+            Guid userId = default)
+        {
+            var grantApplicationRolesViewModel = new GrantApplicationRolesViewModel()
+            {
+                UserId = userId,
+                ApplicationRoleIds = applicationRoleIds
+            };
+
+            return grantApplicationRolesViewModel;
+        }
+    }
+}
