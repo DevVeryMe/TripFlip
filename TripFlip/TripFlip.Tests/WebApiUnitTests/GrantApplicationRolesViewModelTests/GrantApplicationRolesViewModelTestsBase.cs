ad9bac0bad0b3239c90a1a9ab5af0628d48c1f5b
Andriy Veremiy <andrewveremiyy@gmail.com>
2020-09-22 01:44:02 +0300

    [TN202B-241] Added summary.

--- TripFlip/TripFlip.Tests/WebApiUnitTests/GrantApplicationRolesViewModelTests/GrantApplicationRolesViewModelTestsBase.cs
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/GrantApplicationRolesViewModelTests/GrantApplicationRolesViewModelTestsBase.cs
@@ -6,6 +6,13 @@ namespace WebApiUnitTests.GrantApplicationRolesViewModelTests
 {
     public abstract class GrantApplicationRolesViewModelTestsBase
     {
+        /// <summary>
+        /// Creates GrantApplicationRolesViewModel object
+        /// with given user id and application roles' ids.
+        /// </summary>
+        /// <param name="applicationRoleIds">Application roles' ids.</param>
+        /// <param name="userId">User id.</param>
+        /// <returns></returns>
         protected static GrantApplicationRolesViewModel BuildGrantApplicationRolesViewModel(
             IEnumerable<int> applicationRoleIds,
             Guid userId = default)
