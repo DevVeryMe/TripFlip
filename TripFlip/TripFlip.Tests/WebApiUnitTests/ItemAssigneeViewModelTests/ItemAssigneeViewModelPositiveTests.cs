ac870711210932dff08e4a9f42afc8a0b393ef21
Andriy Veremiy <andrewveremiyy@gmail.com>
2020-09-21 23:42:18 +0300

    [TN202B-238] Implement Unit tests for ItemAssignees view model.

---
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/ItemAssigneeViewModelTests/ItemAssigneeViewModelPositiveTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using WebApiUnitTests.Helpers;
+
+namespace WebApiUnitTests.ItemAssigneeViewModelTests
+{
+    [TestClass]
+    public class ItemAssigneeViewModelPositiveTests
+        : ItemAssigneeViewModelTestsBase
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetValidItemIdData), DynamicDataSourceType.Method)]
+        public void ItemId_Validation_Successful(string displayName,
+            int validItemId)
+        {
+            // Arrange.
+            var validRouteSubscriberIds = new int[] { 1, 2, 3 };
+
+            var itemAssigneesViewModel =
+                BuildItemAssigneesViewModel(validRouteSubscriberIds, validItemId);
+
+            // Act.
+            bool modelIsValid = ModelValidator.IsValid(itemAssigneesViewModel);
+
+            // Assert.
+            Assert.IsTrue(modelIsValid, displayName);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetValidRouteSubscriberIdsData), DynamicDataSourceType.Method)]
+        public void RouteSubscriberIds_Validation_Successful(string displayName,
+            IEnumerable<int> validRouteSubscriberIds)
+        {
+            // Arrange.
+            var itemAssigneesViewModel =
+                BuildItemAssigneesViewModel(routeSubscriberIds: validRouteSubscriberIds);
+
+            // Act.
+            bool modelIsValid = ModelValidator.IsValid(itemAssigneesViewModel);
+
+            // Assert.
+            Assert.IsTrue(modelIsValid, displayName);
+        }
+
+        private static IEnumerable<object[]> GetValidItemIdData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Build ItemAssigneeViewModel object" +
+                " and set ItemId field with MAX allowed valid value.",
+                int.MaxValue
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: Build ItemAssigneeViewModel object" +
+                " and set ItemId field with MIN allowed valid value.",
+                1
+            };
+
+            yield return new object[]
+            {
+                "Test case 3: Build ItemAssigneeViewModel object" +
+                " and set ItemId field with simple valid value.",
+                100
+            };
+        }
+
+        private static IEnumerable<object[]> GetValidRouteSubscriberIdsData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Build ItemAssigneeViewModel object" +
+                " and add to RouteSubscriberIds array MAX allowed valid value.",
+                new int[] { int.MaxValue }
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: Build ItemAssigneeViewModel object" +
+                " and add to RouteSubscriberIds array MIN allowed valid value.",
+                new int[] { 1 }
+            };
+
+            yield return new object[]
+            {
+                "Test case 3: Build ItemAssigneeViewModel object" +
+                " and fill RouteSubscriberIds array with simple valid value.",
+                new int[] { 2, 3, 4 }
+            };
+
+            yield return new object[]
+            {
+                "Test case 4: Build ItemAssigneeViewModel object" +
+                " and make RouteSubscriberIds array empty.",
+                new int[] { }
+            };
+        }
+    }
+}
---
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/ItemAssigneeViewModelTests/ItemAssigneeViewModelTestsBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using TripFlip.ViewModels.ItemViewModels;
+
+namespace WebApiUnitTests.ItemAssigneeViewModelTests
+{
+    public abstract class ItemAssigneeViewModelTestsBase
+    {
+        /// <summary>
+        /// Creates ItemAssigneesViewModel object with given Item id
+        /// and Route subscribers' ids.
+        /// </summary>
+        /// <param name="routeSubscriberIds">Route subscribers' ids.</param>
+        /// <param name="itemId">Item id.</param>
+        /// <returns>Created ItemAssigneesViewModel object.</returns>
+        protected static ItemAssigneesViewModel BuildItemAssigneesViewModel(
+            IEnumerable<int> routeSubscriberIds,
+            int itemId = 3)
+        {
+            var itemAssigneesViewModel = new ItemAssigneesViewModel()
+            {
+                ItemId = itemId,
+                RouteSubscriberIds = routeSubscriberIds
+            };
+
+            return itemAssigneesViewModel;
+        }
+    }
+}
