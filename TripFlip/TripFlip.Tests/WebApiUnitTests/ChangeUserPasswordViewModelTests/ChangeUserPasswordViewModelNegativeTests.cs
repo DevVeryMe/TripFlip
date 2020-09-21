ed0f61854bbd3df694240a45208cd337a8d97090
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-09-21 21:13:20 +0300

    [TN202B-240] Implement unit tests for ChangeUserPasswordViewModel.

---
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/ChangeUserPasswordViewModelTests/ChangeUserPasswordViewModelNegativeTests.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TripFlip.ViewModels.UserViewModels;
+
+namespace WebApiUnitTests.ChangeUserPasswordViewModelTests
+{
+    [TestClass]
+    public class ChangeUserPasswordViewModelNegativeTests
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestOldPasswordData), DynamicDataSourceType.Method)]
+        public void TestOldPasswordValidation(
+            string testCaseDisplayName,
+            string oldPassword)
+        {
+            // Arrange
+            var changeUserPasswordViewModel =
+                GetChangeUserPasswordViewModel(oldPassword: oldPassword);
+
+            // Act
+            var result = Validator.TryValidateObject(changeUserPasswordViewModel,
+                new ValidationContext(changeUserPasswordViewModel, null, null),
+                null,
+                true);
+
+            // Assert
+            Assert.IsFalse(result, testCaseDisplayName);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestNewPasswordData), DynamicDataSourceType.Method)]
+        public void TestNewPasswordValidation(
+            string testCaseDisplayName,
+            string newPassword,
+            string newPasswordConfirmation)
+        {
+            // Arrange
+            var changeUserPasswordViewModel =
+                GetChangeUserPasswordViewModel(newPassword: newPassword, 
+                    newPasswordConfirmation: newPasswordConfirmation);
+
+            // Act
+            var result = Validator.TryValidateObject(changeUserPasswordViewModel,
+                new ValidationContext(changeUserPasswordViewModel, null, null),
+                null,
+                true);
+
+            // Assert
+            Assert.IsFalse(result, testCaseDisplayName);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestNewPasswordConfirmationData), DynamicDataSourceType.Method)]
+        public void TestNewPasswordConfirmationValidation(
+            string testCaseDisplayName,
+            string newPassword,
+            string newPasswordConfirmation)
+        {
+            // Arrange
+            var changeUserPasswordViewModel =
+                GetChangeUserPasswordViewModel(newPassword: newPassword,
+                    newPasswordConfirmation: newPasswordConfirmation);
+
+            // Act
+            var result = Validator.TryValidateObject(changeUserPasswordViewModel,
+                new ValidationContext(changeUserPasswordViewModel, null, null),
+                null,
+                true);
+
+            // Assert
+            Assert.IsFalse(result, testCaseDisplayName);
+        }
+
+        static IEnumerable<object[]> GetTestOldPasswordData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Test_ChangeUserPassword_Validation_Given_invalid_" +
+                "OldPassword_equals_null_Validation_should_be_failed.",
+                null
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: Test_ChangeUserPassword_Validation_Given_invalid_OldPassword_equals" +
+                "_empty_string_Validation_should_be_failed.",
+                string.Empty
+            };
+
+            yield return new object[]
+            {
+                "Test case 3: Test_ChangeUserPassword_Validation_Given_invalid_OldPassword_" +
+                "too_long_length_Validation_should_be_failed.",
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaa1@" +
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaA"
+            };
+
+            yield return new object[]
+            {
+                "Test case 4: Test_ChangeUserPassword_Validation_Given_invalid_OldPassword_" +
+                "too_short_length_Validation_should_be_failed.",
+                "Aaaaa1@"
+            };
+
+            yield return new object[]
+            {
+                "Test case 5: Test_ChangeUserPassword_Validation_Given_invalid_OldPassword_" +
+                "incorrect_format_Validation_should_be_failed.",
+                new string('*', 10)
+            };
+        }
+
+        static IEnumerable<object[]> GetTestNewPasswordData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Test_ChangeUserPassword_Validation_Given_invalid_" +
+                "NewPassword_equals_null_Validation_should_be_failed.",
+                null,
+                null
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: Test_ChangeUserPassword_Validation_Given_invalid_NewPassword_equals" +
+                "_empty_string_Validation_should_be_failed.",
+                string.Empty,
+                string.Empty,
+            };
+
+            yield return new object[]
+            {
+                "Test case 3: Test_ChangeUserPassword_Validation_Given_invalid_NewPassword_" +
+                "too_long_length_Validation_should_be_failed.",
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaa1@" +
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaA",
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaa1@" +
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaA"
+            };
+
+            yield return new object[]
+            {
+                "Test case 4: Test_ChangeUserPassword_Validation_Given_invalid_NewPassword_" +
+                "too_short_length_Validation_should_be_failed.",
+                "Aaaaa1@",
+                "Aaaaa1@"
+            };
+
+            yield return new object[]
+            {
+                "Test case 5: Test_ChangeUserPassword_Validation_Given_invalid_NewPassword_" +
+                "incorrect_format_Validation_should_be_failed.",
+                new string('*', 10),
+                new string('*', 10)
+            };
+        }
+
+        static IEnumerable<object[]> GetTestNewPasswordConfirmationData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Test_ChangeUserPassword_Validation_Given_invalid_" +
+                "NewPasswordConfirmation_equals_null_Validation_should_be_failed.",
+                null,
+                null
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: Test_ChangeUserPassword_Validation_Given_invalid_" +
+                "NewPasswordConfirmation_equals_empty_string_Validation_should_be_failed.",
+                string.Empty,
+                string.Empty,
+            };
+
+            yield return new object[]
+            {
+                "Test case 3: Test_ChangeUserPassword_Validation_Given_invalid_" +
+                "NewPasswordConfirmation_too_long_length_Validation_should_be_failed.",
+                new string('*', 101),
+                new string('*', 101)
+            };
+
+            yield return new object[]
+            {
+                "Test case 4: Test_ChangeUserPassword_Validation_Given_invalid_" +
+                "NewPasswordConfirmation_too_short_length_Validation_should_be_failed.",
+                new string('*', 7),
+                new string('*', 7)
+            };
+
+            yield return new object[]
+            {
+                "Test case 4: Test_ChangeUserPassword_Validation_Given_invalid_" +
+                "NewPasswordConfirmation_missmatch_with_NewPassword_Validation_should_be_failed.",
+                "Aaaaaa1@",
+                new string('*', 8)
+            };
+        }
+
+        private ChangeUserPasswordViewModel GetChangeUserPasswordViewModel(
+            string oldPassword = "String1@string", string newPassword = "string1@String", 
+            string newPasswordConfirmation = "string1@String")
+        {
+            return new ChangeUserPasswordViewModel()
+            {
+                OldPassword = oldPassword,
+                NewPassword = newPassword,
+                NewPasswordConfirmation = newPasswordConfirmation
+            };
+        }
+    }
+}
---
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/ChangeUserPasswordViewModelTests/ChangeUserPasswordViewModelPositiveTests.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TripFlip.ViewModels.UserViewModels;
+
+namespace WebApiUnitTests.ChangeUserPasswordViewModelTests
+{
+    [TestClass]
+    public class ChangeUserPasswordViewModelPositiveTests
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestOldPasswordData), DynamicDataSourceType.Method)]
+        public void TestOldPasswordValidation(
+            string testCaseDisplayName,
+            string oldPassword)
+        {
+            // Arrange
+            var changeUserPasswordViewModel =
+                GetChangeUserPasswordViewModel(oldPassword: oldPassword);
+
+            // Act
+            var result = Validator.TryValidateObject(changeUserPasswordViewModel,
+                new ValidationContext(changeUserPasswordViewModel, null, null),
+                null,
+                true);
+
+            // Assert
+            Assert.IsTrue(result, testCaseDisplayName);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestNewPasswordData), DynamicDataSourceType.Method)]
+        public void TestNewPasswordValidation(
+            string testCaseDisplayName,
+            string newPassword,
+            string newPasswordConfirmation)
+        {
+            // Arrange
+            var changeUserPasswordViewModel =
+                GetChangeUserPasswordViewModel(newPassword: newPassword, 
+                    newPasswordConfirmation: newPasswordConfirmation);
+
+            // Act
+            var result = Validator.TryValidateObject(changeUserPasswordViewModel,
+                new ValidationContext(changeUserPasswordViewModel, null, null),
+                null,
+                true);
+
+            // Assert
+            Assert.IsTrue(result, testCaseDisplayName);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestNewPasswordConfirmationData), DynamicDataSourceType.Method)]
+        public void TestNewPasswordConfirmationValidation(
+            string testCaseDisplayName,
+            string newPassword,
+            string newPasswordConfirmation)
+        {
+            // Arrange
+            var changeUserPasswordViewModel =
+                GetChangeUserPasswordViewModel(newPassword: newPassword,
+                    newPasswordConfirmation: newPasswordConfirmation);
+
+            // Act
+            var result = Validator.TryValidateObject(changeUserPasswordViewModel,
+                new ValidationContext(changeUserPasswordViewModel, null, null),
+                null,
+                true);
+
+            // Assert
+            Assert.IsTrue(result, testCaseDisplayName);
+        }
+
+        static IEnumerable<object[]> GetTestOldPasswordData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Test_ChangeUserPassword_Validation_Given_valid_OldPassword_equals" +
+                "_minimum_value_8_and_correct_format_Validation_should_be_successful.",
+                "Aaaaaa1@"
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: Test_ChangeUserPassword_Validation_Given_valid_OldPassword_" +
+                "equals_maximum_value_100_and_correct_format_Validation_should_be_successful.",
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaa1@" +
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaa"
+            };
+
+            yield return new object[]
+            {
+                "Test case 3: Test_ChangeUserPassword_Validation_Given_valid_OldPassword_" +
+                "equals_average_value_50_and_correct_format_Validation_should_be_successful.",
+                "AaaaaaaaaaAaaaaaaa1@AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaA"
+            };
+        }
+
+        static IEnumerable<object[]> GetTestNewPasswordData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Test_ChangeUserPassword_Validation_Given_valid_NewPassword_equals" +
+                "_minimum_value_8_and_correct_format_Validation_should_be_successful.",
+                "Aaaaaa1@",
+                "Aaaaaa1@"
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: Test_ChangeUserPassword_Validation_Given_valid_NewPassword_" +
+                "equals_maximum_value_100_and_correct_format_Validation_should_be_successful.",
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaa1@" +
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaa",
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaa1@" +
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaa"
+            };
+
+            yield return new object[]
+            {
+                "Test case 3: Test_ChangeUserPassword_Validation_Given_valid_NewPassword_" +
+                "equals_average_value_50_and_correct_format_Validation_should_be_successful.",
+                "AaaaaaaaaaAaaaaaaa1@AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaA",
+                "AaaaaaaaaaAaaaaaaa1@AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaA"
+            };
+        }
+
+        static IEnumerable<object[]> GetTestNewPasswordConfirmationData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: Test_ChangeUserPassword_Validation_Given_valid_NewPasswordConfirmation_equals" +
+                "_minimum_value_8_and_matches_NewPassword_Validation_should_be_successful.",
+                "Aaaaaa1@",
+                "Aaaaaa1@"
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: Test_ChangeUserPassword_Validation_Given_valid_NewPasswordConfirmation_" +
+                "equals_maximum_value_100_and_and_matches_NewPassword_Validation_should_be_successful.",
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaa1@" +
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaa",
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaaaaAaaaaaaa1@" +
+                "AaaaaaaaaaAaaaaaaaaaAaaaaaaaaa"
+            };
+
+            yield return new object[]
+            {
+                "Test case 3: Test_ChangeUserPassword_Validation_Given_valid_NewPasswordConfirmation_" +
+                "equals_average_value_50_and_matches_NewPassword_Validation_should_be_successful.",
+                "AaaaaaaaaaAaaaaaaa1@AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaA",
+                "AaaaaaaaaaAaaaaaaa1@AaaaaaaaaaAaaaaaaaaaAaaaaaaaaaA"
+            };
+        }
+
+        private ChangeUserPasswordViewModel GetChangeUserPasswordViewModel(
+            string oldPassword = "String1@string", string newPassword = "string1@String",
+            string newPasswordConfirmation = "string1@String")
+        {
+            return new ChangeUserPasswordViewModel()
+            {
+                OldPassword = oldPassword,
+                NewPassword = newPassword,
+                NewPasswordConfirmation = newPasswordConfirmation
+            };
+        }
+    }
+}
