ace599443eb0c6650d624d38ff4cce381ae55f5d
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-09-23 19:14:16 +0300

    [TN202B-249] Implement ItemListDtoComparer.

--- TripFlip/TripFlip.Tests/WebApiIntegrationTests/CustomComparers/ItemListDtoComparer.cs
+++ TripFlip/TripFlip.Tests/WebApiIntegrationTests/CustomComparers/ItemListDtoComparer.cs
@@ -1,6 +1,49 @@
-﻿namespace WebApiIntegrationTests.CustomComparers
+﻿using System;
+using System.Collections.Generic;
+using TripFlip.Services.Dto.ItemListDtos;
+
+namespace WebApiIntegrationTests.CustomComparers
 {
-    public class ItemListDtoComparer
+    public class ItemListDtoComparer : IComparer<ItemListDto>
     {
+        public int Compare(ItemListDto x, ItemListDto y)
+        {
+            if (x == null && y == null)
+            {
+                return 0;
+            }
+
+            if (x == null)
+            {
+                return -1;
+            }
+
+            if (y == null)
+            {
+                return 1;
+            }
+
+            if (x.Id.CompareTo(y.Id) != 0)
+            {
+                return x.Id.CompareTo(y.Id);
+            }
+
+            if (x.RouteId.CompareTo(y.RouteId) != 0)
+            {
+                return x.Id.CompareTo(y.Id);
+            }
+
+            if (string.Compare(x.Title, y.Title, StringComparison.Ordinal) != 0)
+            {
+                return string.Compare(x.Title, y.Title, StringComparison.Ordinal);
+            }
+
+            if (DateTimeOffset.Compare(x.DateCreated, y.DateCreated) != 0)
+            {
+                return DateTimeOffset.Compare(x.DateCreated, y.DateCreated);
+            }
+
+            return 0;
+        }
     }
 }
