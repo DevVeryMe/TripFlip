4cb37896b545c344bf5843528da7bb31febc9457
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-09-24 17:16:51 +0300

    [TN202B-249] Remove redundant new line and namespaces.

--- TripFlip/TripFlip.Tests/WebApiIntegrationTests/ItemListServiceTests/ItemListServiceNegativeTests.cs
+++ TripFlip/TripFlip.Tests/WebApiIntegrationTests/ItemListServiceTests/ItemListServiceNegativeTests.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using TripFlip.Services;
 using TripFlip.Services.CustomExceptions;
