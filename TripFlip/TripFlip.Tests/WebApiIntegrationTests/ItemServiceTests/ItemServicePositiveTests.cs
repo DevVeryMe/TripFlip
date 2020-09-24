67156c8cfe490e81fed7c2aed753174f1ba1ba67
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-09-24 20:11:47 +0300

    [TN202B-254] Return accidentally deleted CreateAsync_ValidData_Successful. Rename positive test methods.

--- TripFlip/TripFlip.Tests/WebApiIntegrationTests/ItemServiceTests/ItemServicePositiveTests.cs
+++ TripFlip/TripFlip.Tests/WebApiIntegrationTests/ItemServiceTests/ItemServicePositiveTests.cs
@@ -42,7 +42,32 @@ namespace WebApiIntegrationTests.ItemServiceTests
         }
 
         [TestMethod]
-        public async Task CreateItem_ValidData_Successful()
+        public async Task CreateAsync_ValidData_Successful()
+        {
+            Seed(TripFlipDbContext, ValidUser);
+            Seed(TripFlipDbContext, TripEntityToSeed);
+            Seed(TripFlipDbContext, RouteEntityToSeed);
+            Seed(TripFlipDbContext, ItemListEntityToSeed);
+            Seed(TripFlipDbContext, TripSubscriberEntitiesToSeed);
+            Seed(TripFlipDbContext, RouteSubscriberEntitiesToSeed);
+            Seed(TripFlipDbContext, RouteRoleEntitiesToSeed);
+            Seed(TripFlipDbContext, RouteSubscriberRoleEntitiesToSeed);
+
+            CurrentUserService = CreateCurrentUserService(ValidUser.Id,
+                ValidUser.Email);
+
+            var createItemDto = GetCreateItemDto();
+            var itemService = new ItemService(Mapper, TripFlipDbContext,
+                CurrentUserService);
+            var resultItemDto = await itemService.CreateAsync(createItemDto);
+            var comparer = new ItemDtoComparer();
+
+            Assert.AreEqual(0,
+                comparer.Compare(_expectedCreatedItemDto, resultItemDto));
+        }
+
+        [TestMethod]
+        public async Task UpdateAsync_ValidData_Successful()
         {
             Seed(TripFlipDbContext, ValidUser);
             Seed(TripFlipDbContext, TripEntityToSeed);
