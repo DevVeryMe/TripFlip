ff6c72417b28eb88f755d16b9a203febf436d20f
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-09-24 19:40:36 +0300

    [TN202B-259] Implement negative tests for GetByIdAsync method of RouteService.

--- TripFlip/TripFlip.Tests/WebApiIntegrationTests/RouteServiceTests/RouteServiceNegativeTests.cs
+++ TripFlip/TripFlip.Tests/WebApiIntegrationTests/RouteServiceTests/RouteServiceNegativeTests.cs
@@ -1,6 +1,37 @@
-﻿namespace WebApiIntegrationTests.RouteServiceTests
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using TripFlip.Services;
+using TripFlip.Services.CustomExceptions;
+
+namespace WebApiIntegrationTests.RouteServiceTests
 {
+    [TestClass]
     public class RouteServiceNegativeTests : TestRouteServiceBase
     {
+        [TestInitialize]
+        public void Initialize()
+        {
+            TripFlipDbContext = CreateDbContext();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            TripFlipDbContext.Dispose();
+        }
+
+        [TestMethod]
+        public async Task Test_GetByIdAsync_GivenNotValidId_ExceptionThrown()
+        {
+            Seed(TripFlipDbContext, TripEntityToSeed);
+            Seed(TripFlipDbContext, RouteEntityToSeed);
+
+            var invalidId = 2;
+
+            var routeService = new RouteService(TripFlipDbContext, Mapper);
+
+            await Assert.ThrowsExceptionAsync<NotFoundException>(async () =>
+                await routeService.GetByIdAsync(invalidId));
+        }
     }
 }
--- TripFlip/TripFlip.Tests/WebApiIntegrationTests/RouteServiceTests/TestRouteServiceBase.cs
+++ TripFlip/TripFlip.Tests/WebApiIntegrationTests/RouteServiceTests/TestRouteServiceBase.cs
@@ -1,6 +1,43 @@
-﻿namespace WebApiIntegrationTests.RouteServiceTests
+﻿using Moq;
+using System;
+using System.Globalization;
+using TripFlip.Domain.Entities;
+using TripFlip.Services.Interfaces;
+
+namespace WebApiIntegrationTests.RouteServiceTests
 {
     public class TestRouteServiceBase : TestServiceBase
     {
+        protected static TripEntity TripEntityToSeed => new TripEntity()
+        {
+            Id = 1,
+            Title = "Trip",
+            Description = "Description",
+            StartsAt = DateTimeOffset.Parse("28/08/2030 14:00:00",
+                CultureInfo.GetCultureInfo("en-GB").DateTimeFormat),
+            EndsAt = DateTimeOffset.Parse("30/11/2030 19:00:00",
+                CultureInfo.GetCultureInfo("en-GB").DateTimeFormat)
+        };
+
+        protected RouteEntity RouteEntityToSeed => new RouteEntity()
+        {
+            Id = 1,
+            Title = "Route",
+            TripId = 1,
+        };
+
+        protected ICurrentUserService CurrentUserService;
+
+        protected static ICurrentUserService CreateCurrentUserService(Guid id, string email)
+        {
+            var correctEmail = email;
+            var correctGuid = id;
+
+            var mock = new Mock<ICurrentUserService>();
+            mock.Setup(a => a.UserEmail).Returns(correctEmail);
+            mock.Setup(a => a.UserId).Returns(correctGuid);
+
+            return mock.Object;
+        }
     }
 }
