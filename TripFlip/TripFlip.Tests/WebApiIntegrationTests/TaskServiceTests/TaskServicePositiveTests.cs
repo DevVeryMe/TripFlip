f61faf0aeea6f5fcb8cce18fbe226aec348c88c4
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-09-24 20:36:43 +0300

    [TN202B-268] Implement positive test for getting by id task.

--- TripFlip/TripFlip.Tests/WebApiIntegrationTests/TaskServiceTests/TaskServicePositiveTests.cs
+++ TripFlip/TripFlip.Tests/WebApiIntegrationTests/TaskServiceTests/TaskServicePositiveTests.cs
@@ -1,6 +1,56 @@
-﻿namespace WebApiIntegrationTests.TaskServiceTests
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using TripFlip.Services;
+using TripFlip.Services.Dto.Enums;
+using TripFlip.Services.Dto.TaskDtos;
+using WebApiIntegrationTests.CustomComparers;
+
+namespace WebApiIntegrationTests.TaskServiceTests
 {
-    public class TaskServicePositiveTests
+    [TestClass]
+    public class TaskServicePositiveTests : TestTaskServiceBase
     {
+        private readonly TaskDto _expectedGotByIdRouteDto = new TaskDto()
+        {
+            Id = 1,
+            Description = "Task",
+            IsCompleted = false,
+            PriorityLevel = TaskPriorityLevel.Low,
+            TaskListId = 1
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            TripFlipDbContext = CreateDbContext();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            TripFlipDbContext.Dispose();
+        }
+
+        [TestMethod]
+        public async Task GetById_GivenValidId_Successful()
+        {
+            Seed(TripFlipDbContext, TripEntityToSeed);
+            Seed(TripFlipDbContext, RouteEntityToSeed);
+            Seed(TripFlipDbContext, TaskListEntityToSeed);
+            Seed(TripFlipDbContext, TaskEntityToSeed);
+
+            var validTaskId = 1;
+
+            CurrentUserService = CreateCurrentUserService(ValidUser.Id,
+                ValidUser.Email);
+
+            var taskService = new TaskService(TripFlipDbContext, Mapper,
+                CurrentUserService);
+
+            var resultTaskDto = await taskService.GetByIdAsync(validTaskId);
+            var compaper = new TaskDtoComparer();
+
+            Assert.AreEqual(0, compaper.Compare(_expectedGotByIdRouteDto, resultTaskDto));
+        }
     }
 }
