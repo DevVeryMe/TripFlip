65d702758a1bbbcc74e5c531eb0c6d7119e1e49d
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-09-25 20:26:41 +0300

    [TN202B-262] Add seed data, implement negative test.

--- TripFlip/TripFlip.Tests/WebApiIntegrationTests/TaskListServiceTests/TaskListServiceNegativeTests.cs
+++ TripFlip/TripFlip.Tests/WebApiIntegrationTests/TaskListServiceTests/TaskListServiceNegativeTests.cs
@@ -1,6 +1,54 @@
-﻿namespace WebApiIntegrationTests.TaskListServiceTests
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using TripFlip.Services;
+using TripFlip.Services.CustomExceptions;
+using TripFlip.Services.Dto;
+
+namespace WebApiIntegrationTests.TaskListServiceTests
 {
+    [TestClass]
     public class TaskListServiceNegativeTests : TestTaskListServiceBase
     {
+        [TestInitialize]
+        public void Initialize()
+        {
+            TripFlipDbContext = CreateDbContext();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            TripFlipDbContext.Dispose();
+        }
+
+        [TestMethod]
+        public async Task GetAllByRouteIdAsync_GivenInvalidRouteId_ExceptionThrown()
+        {
+            Seed(TripFlipDbContext, TripEntityToSeed);
+            Seed(TripFlipDbContext, RouteEntityToSeed);
+            Seed(TripFlipDbContext, TaskListEntitiesToSeed);
+
+            CurrentUserService = CreateCurrentUserService(ValidUser.Id, ValidUser.Email);
+
+            var taskListService = new TaskListService(TripFlipDbContext, 
+                Mapper, CurrentUserService);
+            var invalidRouteId = 2;
+            var paginationDto = GetPaginationDto();
+            string searchString = null;
+
+            await Assert.ThrowsExceptionAsync<NotFoundException>(async () =>
+                await taskListService.GetAllByRouteIdAsync(invalidRouteId, 
+                    searchString, paginationDto));
+        }
+
+        private PaginationDto GetPaginationDto(int? pageNumber = null,
+            int? pageSize = null)
+        {
+            return new PaginationDto()
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+        }
     }
 }
--- TripFlip/TripFlip.Tests/WebApiIntegrationTests/TaskListServiceTests/TestTaskListServiceBase.cs
+++ TripFlip/TripFlip.Tests/WebApiIntegrationTests/TaskListServiceTests/TestTaskListServiceBase.cs
@@ -1,11 +1,61 @@
 ﻿using Moq;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using TripFlip.Domain.Entities;
 using TripFlip.Services.Interfaces;
 
 namespace WebApiIntegrationTests.TaskListServiceTests
 {
     public class TestTaskListServiceBase : TestServiceBase
     {
+        protected static TripEntity TripEntityToSeed => new TripEntity()
+        {
+            Id = 1,
+            Title = "Trip",
+            Description = "Description",
+            StartsAt = DateTimeOffset.Parse("28/08/2030 14:00:00",
+                CultureInfo.GetCultureInfo("en-GB").DateTimeFormat),
+            EndsAt = DateTimeOffset.Parse("30/11/2030 19:00:00",
+                CultureInfo.GetCultureInfo("en-GB").DateTimeFormat)
+        };
+
+        protected RouteEntity RouteEntityToSeed => new RouteEntity()
+        {
+            Id = 1,
+            Title = "Route",
+            TripId = 1,
+        };
+
+        protected IEnumerable<TaskListEntity> TaskListEntitiesToSeed =>
+            new List<TaskListEntity>()
+            {
+                new TaskListEntity()
+                {
+                    Id = 1,
+                    RouteId = 1,
+                    Title = "Task list 1"
+                },
+                new TaskListEntity()
+                {
+                    Id = 2,
+                    RouteId = 1,
+                    Title = "Task list 2"
+                },
+                new TaskListEntity()
+                {
+                    Id = 3,
+                    RouteId = 1,
+                    Title = "Task list 3"
+                }
+            };
+
+        protected static UserEntity ValidUser => new UserEntity()
+        {
+            Id = Guid.Parse("0f8fad5b-d9cb-469f-a165-70867728950e"),
+            Email = "correct@mail.com"
+        };
+
         protected ICurrentUserService CurrentUserService;
 
         protected static ICurrentUserService CreateCurrentUserService(Guid id, string email)
