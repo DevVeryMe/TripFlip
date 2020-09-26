a7e7b28787ecd8bb77a57991d9226c055c6ff4aa
Andriy Veremiy <andrewveremiyy@gmail.com>
2020-09-26 16:52:43 +0300

    [TN202B-269] Implemented negative tests for create task.

--- TripFlip/TripFlip.Tests/WebApiIntegrationTests/TaskServiceTests/TaskServiceNegativeTests.cs
+++ TripFlip/TripFlip.Tests/WebApiIntegrationTests/TaskServiceTests/TaskServiceNegativeTests.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TripFlip.Services;
 using TripFlip.Services.CustomExceptions;
+using TripFlip.Services.Interfaces;
 
 namespace WebApiIntegrationTests.TaskServiceTests
 {
@@ -23,7 +26,7 @@ namespace WebApiIntegrationTests.TaskServiceTests
         [TestMethod]
         public async Task GetByIdAsync_GivenNotValidId_ExceptionThrown()
         {
-            // Arrange
+            // Arrange.
             var invalidId = 2;
 
             CurrentUserService = CreateCurrentUserService(ValidUser.Id,
@@ -31,9 +34,104 @@ namespace WebApiIntegrationTests.TaskServiceTests
             var taskService = new TaskService(TripFlipDbContext, Mapper,
                 CurrentUserService);
 
-            // Act + Assert
+            // Act + Assert.
             await Assert.ThrowsExceptionAsync<NotFoundException>(async () =>
                 await taskService.GetByIdAsync(invalidId));
+        }
+
+        [TestMethod]
+        public async Task CreateAsync_GivenNonExistentTaskListId_ExceptionThrown()
+        {
+            // Arrange.
+            Seed(TripFlipDbContext, ValidUser);
+            Seed(TripFlipDbContext, TripEntityToSeed);
+            Seed(TripFlipDbContext, RouteEntityToSeed);
+            Seed(TripFlipDbContext, TaskListEntityToSeed);
+
+            CurrentUserService = CreateCurrentUserService(ValidUser.Id,
+                ValidUser.Email);
+
+            var nonExistentTaskListId = 2;
+            var createTaskDto = GetCreateTaskDto(taskListId: nonExistentTaskListId);
+            var taskService = new TaskService(TripFlipDbContext, Mapper, CurrentUserService);
+
+            // Act + Assert.
+            await Assert.ThrowsExceptionAsync<NotFoundException>(async () =>
+                await taskService.CreateAsync(createTaskDto));
+        }
+
+        [TestMethod]
+        public async Task CreateAsync_GivenCurrentUserNotRouteAdmin_ExceptionThrown()
+        {
+            // Arrange.
+            Seed(TripFlipDbContext, ValidUser);
+            Seed(TripFlipDbContext, TripEntityToSeed);
+            Seed(TripFlipDbContext, RouteEntityToSeed);
+            Seed(TripFlipDbContext, TaskListEntityToSeed);
+            Seed(TripFlipDbContext, TripSubscriberEntitiesToSeed);
+            Seed(TripFlipDbContext, RouteSubscriberEntitiesToSeed);
+            Seed(TripFlipDbContext, RouteRoleEntitiesToSeed);
+
+            CurrentUserService = CreateCurrentUserService(ValidUser.Id,
+                ValidUser.Email);
+
+            var createTaskDto = GetCreateTaskDto();
+            var taskService = new TaskService(TripFlipDbContext, Mapper, CurrentUserService);
+
+            // Act + Assert.
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () =>
+                await taskService.CreateAsync(createTaskDto));
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetCurrentUserServiceInvalidData), DynamicDataSourceType.Method)]
+        public async Task CreateAsync_GivenNotValidCurrentUser_ExceptionThrown(
+            string displayName, ICurrentUserService currentUserService)
+        {
+            // Arrange.
+            Seed(TripFlipDbContext, NonExistentUser);
+            Seed(TripFlipDbContext, NotRouteSubscriberUser);
+            Seed(TripFlipDbContext, NotTripSubscriberUser);
+
+            Seed(TripFlipDbContext, TripEntityToSeed);
+            Seed(TripFlipDbContext, RouteEntityToSeed);
+            Seed(TripFlipDbContext, TripSubscriberEntitiesToSeed);
+            Seed(TripFlipDbContext, RouteSubscriberEntitiesToSeed);
+
+            CurrentUserService = currentUserService;
+
+            var createTaskDto = GetCreateTaskDto();
+            var taskService = new TaskService(TripFlipDbContext, Mapper, CurrentUserService);
+
+            // Act + Assert.
+            await Assert.ThrowsExceptionAsync<NotFoundException>(async () =>
+                await taskService.CreateAsync(createTaskDto), displayName);
+        }
+
+        private static IEnumerable<object[]> GetCurrentUserServiceInvalidData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: CreateAsync_GivenNotExistentCurrentUser_ExceptionThrown",
+                CreateCurrentUserService(NonExistentUser.Id,
+                    NonExistentUser.Email)
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: CreateAsync_GivenCurrentUser" +
+                "NotSubscribedToTrip_ExceptionThrown",
+                CreateCurrentUserService(NotTripSubscriberUser.Id,
+                    NotTripSubscriberUser.Email)
+            };
+
+            yield return new object[]
+            {
+                "Test case 3: CreateAsync_GivenCurrentUser" +
+                "NotSubscribedToRoute_ExceptionThrown",
+                CreateCurrentUserService(NotRouteSubscriberUser.Id,
+                    NotRouteSubscriberUser.Email)
+            };
         }
     }
 }
--- TripFlip/TripFlip.Tests/WebApiIntegrationTests/TaskServiceTests/TestTaskServiceBase.cs
+++ TripFlip/TripFlip.Tests/WebApiIntegrationTests/TaskServiceTests/TestTaskServiceBase.cs
@@ -1,8 +1,10 @@
 ﻿using Moq;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using TripFlip.Domain.Entities;
-using TripFlip.Domain.Entities.Enums;
+using TripFlip.Services.Dto.Enums;
+using TripFlip.Services.Dto.TaskDtos;
 using TripFlip.Services.Interfaces;
 
 namespace WebApiIntegrationTests.TaskServiceTests
@@ -15,14 +17,119 @@ namespace WebApiIntegrationTests.TaskServiceTests
             Email = "correct@mail.com"
         };
 
+        protected static UserEntity NonExistentUser => new UserEntity()
+        {
+            Id = Guid.Parse("322967ec-9415-4778-99c6-7f566d1bb8d2"),
+            Email = "nonexistent@mail.com"
+        };
+
+        protected static UserEntity NotTripSubscriberUser => new UserEntity()
+        {
+            Id = Guid.Parse("c44315ef-547e-4366-888a-46d2e057e6f7"),
+            Email = "notsuboftrip@mail.com"
+        };
+
+        protected static UserEntity NotRouteSubscriberUser => new UserEntity()
+        {
+            Id = Guid.Parse("816fe98f-515c-407a-bf66-cc9a908644c1"),
+            Email = "notsubofroute@mail.com"
+        };
+
+        protected static TripEntity TripEntityToSeed => new TripEntity()
+        {
+            Id = 1,
+            Title = "Trip",
+            Description = "Description",
+            StartsAt = DateTimeOffset.Parse("28/08/2030 14:00:00",
+                CultureInfo.GetCultureInfo("en-GB").DateTimeFormat),
+            EndsAt = DateTimeOffset.Parse("30/11/2030 19:00:00",
+                CultureInfo.GetCultureInfo("en-GB").DateTimeFormat)
+        };
+
+        protected RouteEntity RouteEntityToSeed => new RouteEntity()
+        {
+            Id = 1,
+            Title = "Route",
+            TripId = 1,
+        };
+
+        protected TaskListEntity TaskListEntityToSeed => new TaskListEntity()
+        {
+            Id = 1,
+            Title = "ItemList",
+            RouteId = 1
+        };
+
         protected TaskEntity TaskEntityToSeed => new TaskEntity()
         {
             Id = 1,
             Description = "Task",
             IsCompleted = false,
-            PriorityLevel = TaskPriorityLevel.Low,
+            PriorityLevel = TripFlip.Domain.Entities.Enums.TaskPriorityLevel.Low,
             TaskListId = 1
         };
+
+        protected IEnumerable<TripSubscriberEntity> TripSubscriberEntitiesToSeed => 
+            new List<TripSubscriberEntity>() 
+            {
+                new TripSubscriberEntity()
+                {
+                    Id = 1,
+                    TripId = 1,
+                    UserId = Guid.Parse("0f8fad5b-d9cb-469f-a165-70867728950e")
+                },
+                new TripSubscriberEntity()
+                {
+                    Id = 2,
+                    TripId = 1,
+                    UserId = Guid.Parse("816fe98f-515c-407a-bf66-cc9a908644c1")
+                },
+                new TripSubscriberEntity()
+                {
+                    Id = 3,
+                    TripId = 1,
+                    UserId = Guid.Parse("3ed64e6a-0b5c-423b-a1ec-f0d38c9f6846")
+                }
+            };
+
+        protected IEnumerable<RouteSubscriberEntity> RouteSubscriberEntitiesToSeed =>
+            new List<RouteSubscriberEntity>()
+            {
+                new RouteSubscriberEntity()
+                {
+                    Id = 1,
+                    RouteId = 1,
+                    TripSubscriberId = 1
+                },
+                new RouteSubscriberEntity()
+                {
+                    Id = 2,
+                    RouteId = 1,
+                    TripSubscriberId = 3
+                }
+            };
+
+        protected IEnumerable<RouteRoleEntity> RouteRoleEntitiesToSeed =>
+            new List<RouteRoleEntity>()
+            {
+                new RouteRoleEntity()
+                {
+                    Id = 1,
+                    Name = "Admin"
+                },
+                new RouteRoleEntity()
+                {
+                    Id = 2,
+                    Name = "Editor"
+                }
+            };
+
+        protected RouteSubscriberRoleEntity RouteSubscriberAdminRoleEntityToSeed =>
+            new RouteSubscriberRoleEntity()
+            {
+                RouteRoleId = 1,
+                RouteSubscriberId = 1
+            };
 
         protected ICurrentUserService CurrentUserService;
 
@@ -36,6 +143,18 @@ namespace WebApiIntegrationTests.TaskServiceTests
             mock.Setup(a => a.UserId).Returns(correctGuid);
 
             return mock.Object;
+        }
+
+        protected CreateTaskDto GetCreateTaskDto(int taskListId = 1,
+            string description = "Description",
+            TaskPriorityLevel priorityLevel = TaskPriorityLevel.Low)
+        {
+            return new CreateTaskDto()
+            {
+                TaskListId = taskListId,
+                Description = description,
+                PriorityLevel = priorityLevel
+            };
         }
     }
 }
