e50261eb0b4c685ba15fa85cc6213f3b108acddb
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-09-27 14:40:43 +0300

    [TN202B-279] Implement TestEntityValidationHelperBase.

---
+++ TripFlip/TripFlip.Tests/WebApiIntegrationTests/EntityValidationHelperTests/TestEntityValidationHelperBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using TripFlip.Domain.Entities;
+using TripFlip.Services.Interfaces;
+
+namespace WebApiIntegrationTests.EntityValidationHelperTests
+{
+    public abstract class TestEntityValidationHelperBase : TestServiceBase
+    {
+        protected static UserEntity ValidUser => new UserEntity()
+        {
+            Id = Guid.Parse("0f8fad5b-d9cb-469f-a165-70867728950e"),
+            Email = "correct@mail.com"
+        };
+
+        protected static UserEntity NonExistentUser => new UserEntity()
+        {
+            Id = Guid.Parse("322967ec-9415-4778-99c6-7f566d1bb8d2"),
+            Email = "nonexistent@mail.com"
+        };
+
+        protected IEnumerable<ApplicationRoleEntity> ApplicationRoleEntitiesToSeed = 
+            new List<ApplicationRoleEntity>()
+            {
+                new ApplicationRoleEntity()
+                {
+                    Id = 1,
+                    Name = "SuperAdmin"
+                },
+                new ApplicationRoleEntity()
+                {
+                    Id = 2,
+                    Name = "Admin"
+                }
+            };
+
+        protected static ICurrentUserService CreateCurrentUserService(Guid id, string email)
+        {
+            var correctEmail = email;
+            var correctGuid = id;
+
+            var mock = new Mock<ICurrentUserService>();
+            mock.Setup(a => a.UserEmail).Returns(correctEmail);
+            mock.Setup(a => a.UserId).Returns(correctGuid);
+
+            return mock.Object;
+        }
+    }
+}
