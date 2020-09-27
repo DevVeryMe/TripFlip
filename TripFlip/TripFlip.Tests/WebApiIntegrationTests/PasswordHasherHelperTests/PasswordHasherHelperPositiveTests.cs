8cb47d1212a97a67af45efea3be5fd2c05e07e70
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-09-27 15:08:01 +0300

    [TN202B-304] Implement positive test.

---
+++ TripFlip/TripFlip.Tests/WebApiIntegrationTests/PasswordHasherHelperTests/PasswordHasherHelperPositiveTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TripFlip.Domain.Entities;
+using TripFlip.Services.Helpers;
+
+namespace WebApiIntegrationTests.PasswordHasherHelperTests
+{
+    [TestClass]
+    public class PasswordHasherHelperPositiveTests
+    {
+        [TestMethod]
+        public void VerifyPassword_GivenValidData_Successful()
+        {
+            // Arrange.
+            var password = "ValidPass1@";
+            var passwordHash = GetPasswordHash();
+
+            // Act.
+            var result = PasswordHasherHelper.VerifyPassword(password, passwordHash);
+
+            // Assert.
+            Assert.IsTrue(result);
+        }
+
+        private string GetPasswordHash(string password = "ValidPass1@")
+        {
+            var passwordHasher = new PasswordHasher<UserEntity>();
+            var hashedPassword = passwordHasher.HashPassword(user: null, password: password);
+
+            return hashedPassword;
+        }
+    }
+}
