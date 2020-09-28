2f03ff4eb0afe7c0597ee422c3577d2aae0b8921
gingerbread-monster <staslazlegolas@gmail.com>
2020-09-28 18:07:29 +0300

    [TN202B-278] Tests for Helpers for ValidateCurrentUserTripRoleAsync( )

--- TripFlip/TripFlip.Tests/WebApiIntegrationTests/EntityValidationHelperTests/EntityValidationHelperNegativeTests.cs
+++ TripFlip/TripFlip.Tests/WebApiIntegrationTests/EntityValidationHelperTests/EntityValidationHelperNegativeTests.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TripFlip.Services.CustomExceptions;
+using TripFlip.Services.Enums;
 using TripFlip.Services.Helpers;
+using TripFlip.Services.Interfaces;
 
 namespace WebApiIntegrationTests.EntityValidationHelperTests
 {
@@ -46,6 +49,77 @@ namespace WebApiIntegrationTests.EntityValidationHelperTests
             await Assert.ThrowsExceptionAsync<ArgumentException>(async () =>
                 await EntityValidationHelper.ValidateCurrentUserIsSuperAdminAsync(
                     CurrentUserService, TripFlipDbContext));
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetCurrentUserServiceInvalidData), DynamicDataSourceType.Method)]
+        public async Task ValidateCurrentUserTripRoleAsync_GivenNotValidCurrentUser_ExceptionThrown(
+           string displayName, ICurrentUserService currentUserService)
+        {
+            // Arrange
+            Seed(TripFlipDbContext, NonExistentUser);
+            Seed(TripFlipDbContext, NotTripSubscriberUser);
+            Seed(TripFlipDbContext, TripEntityToSeed);
+            Seed(TripFlipDbContext, TripSubscriberEntitiesToSeed);
+
+            CurrentUserService = currentUserService;
+
+            int existingTripId = 1;
+
+            TripRoles validTripRole = TripRoles.Editor;
+
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<NotFoundException>(async () =>
+                await EntityValidationHelper.ValidateCurrentUserTripRoleAsync(
+                    currentUserService: CurrentUserService,
+                    tripFlipDbContext: TripFlipDbContext,
+                    tripId: existingTripId,
+                    tripRoleToValidate: validTripRole,
+                    errorMessage: string.Empty), displayName);
+        }
+
+        [TestMethod]
+        public async Task ValidateCurrentUserTripRoleAsync_GivenInvalidCurrentUserRole_ExceptionThrown()
+        {
+            // Arrange
+            Seed(TripFlipDbContext, ValidUser);
+            Seed(TripFlipDbContext, TripEntityToSeed);
+            Seed(TripFlipDbContext, TripSubscriberEntitiesToSeed);
+            Seed(TripFlipDbContext, TripRolesToSeed);
+            Seed(TripFlipDbContext, TripSubscriberRoleEntitiesToSeed);
+
+            int existingTripId = 1;
+
+            CurrentUserService = CreateCurrentUserService(ValidUser.Id, ValidUser.Email);
+
+            TripRoles roleThatIsExpectedFromCurrentUser = TripRoles.Editor;
+
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () =>
+                await EntityValidationHelper.ValidateCurrentUserTripRoleAsync(
+                    currentUserService: CurrentUserService,
+                    tripFlipDbContext: TripFlipDbContext,
+                    tripId: existingTripId,
+                    tripRoleToValidate: roleThatIsExpectedFromCurrentUser,
+                    errorMessage: string.Empty));
+        }
+
+        static IEnumerable<object[]> GetCurrentUserServiceInvalidData()
+        {
+            yield return new object[]
+            {
+                "Test case 1: CreateAsync_GivenNotExistentCurrentUser_ExceptionThrown",
+                CreateCurrentUserService(NonExistentUser.Id,
+                    NonExistentUser.Email)
+            };
+
+            yield return new object[]
+            {
+                "Test case 2: CreateAsync_GivenCurrentUser" +
+                "NotSubscribedToTrip_ExceptionThrown",
+                CreateCurrentUserService(NotTripSubscriberUser.Id,
+                    NotTripSubscriberUser.Email)
+            };
         }
     }
 }
