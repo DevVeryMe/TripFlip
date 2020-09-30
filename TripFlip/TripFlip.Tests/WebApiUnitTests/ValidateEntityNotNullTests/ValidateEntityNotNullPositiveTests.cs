154dc4cc6b24ea3f5beaefefff99f8ef11afce3f
Andriy Veremiy <andrewveremiyy@gmail.com>
2020-09-30 18:26:45 +0300

    [JTCNB-51] Implemented unit tests for validate entity not null.

---
+++ TripFlip/TripFlip.Tests/WebApiUnitTests/ValidateEntityNotNullTests/ValidateEntityNotNullPositiveTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TripFlip.Services.Helpers;
+
+namespace WebApiUnitTests.ValidateEntityNotNullTests
+{
+    [TestClass]
+    public class ValidateEntityNotNullPositiveTests
+    {
+        [TestMethod]
+        public void ValidateEntityNotNull_EntityIsNotNull_ValidationSuccessful()
+        {
+            // Arrange.
+            object validEntity = new object();
+            string nullErrorMessage = null;
+
+            // Act + Assert.
+            try
+            {
+                EntityValidationHelper.ValidateEntityNotNull(validEntity, nullErrorMessage);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Expected no exception, but got: " + ex.Message);
+            }
+        }
+    }
+}
