9902f25c32e21bb4a4159c05d4fd865de90e75e9
AndrewKravchuk <kravchuuk.aaa@gmail.com>
2020-10-18 13:09:25 +0300

    [JTCNB-72] Add Register and CreateUserEntityWithRandomPassword private methods to be called from login endpoint if user does not exist. Add helper with randomizing password method.

--- TripFlip/TripFlip.Services/GoogleApiUserService.cs
+++ TripFlip/TripFlip.Services/GoogleApiUserService.cs
@@ -1,8 +1,57 @@
-﻿using TripFlip.Services.Interfaces;
+﻿using System.Threading.Tasks;
+using TripFlip.DataAccess;
+using TripFlip.Domain.Entities;
+using TripFlip.Services.Helpers;
+using TripFlip.Services.Interfaces;
 
 namespace TripFlip.Services
 {
+    /// <inheritdoc />
     public class GoogleApiUserService : IGoogleApiUserService
     {
+        private readonly TripFlipDbContext _tripFlipDbContext;
+
+        /// <summary>
+        /// Initializes db context field.
+        /// </summary>
+        /// <param name="tripFlipDbContext">Instance of db context.</param>
+        public GoogleApiUserService(TripFlipDbContext tripFlipDbContext)
+        {
+            _tripFlipDbContext = tripFlipDbContext;
+        }
+
+        /// <summary>
+        /// Registers new user, randomizing his password.
+        /// </summary>
+        /// <param name="email">Email to register user with.</param>
+        /// <returns>Registered user entity.</returns>
+        private async Task<UserEntity> RegisterAsync(string email)
+        {
+            var userToRegister = CreateUserEntityWithRandomPassword(email);
+
+            await _tripFlipDbContext.Users.AddAsync(userToRegister);
+            await _tripFlipDbContext.SaveChangesAsync();
+
+            return userToRegister;
+        }
+
+        /// <summary>
+        /// Creates a user entity with randomized password.
+        /// </summary>
+        /// <param name="email">Email to create user with.</param>
+        /// <returns>Created user entity.</returns>
+        private UserEntity CreateUserEntityWithRandomPassword(string email)
+        {
+            var password = PasswordGeneratorHelper.GeneratePassword(useLowercase: true,
+                useUppercase: true, useNumbers: true, useSpecial: true, passwordSize: 50);
+
+            var passwordHash = PasswordHasherHelper.HashPassword(password);
+
+            return new UserEntity()
+            {
+                Email = email,
+                PasswordHash = passwordHash
+            };
+        }
     }
 }
---
+++ TripFlip/TripFlip.Services/Helpers/PasswordGeneratorHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TripFlip.Services.Helpers
+{
+    public static class PasswordGeneratorHelper
+    {
+        const string LOWER_CASE = "abcdefghijklmnopqursuvwxyz";
+        const string UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string NUMBERS = "123456789";
+        const string SPECIALS = @"!@£$%^&*()#€";
+
+        /// <summary>
+        /// Generates random password.
+        /// </summary>
+        /// <param name="useLowercase">Parameter to note a use of lower case letters.</param>
+        /// <param name="useUppercase">Parameter to note a use of upper case letters.</param>
+        /// <param name="useNumbers">Parameter to note a use of numbers.</param>
+        /// <param name="useSpecial">Parameter to note a use of special symbols.</param>
+        /// <param name="passwordSize">Size of password.</param>
+        /// <returns>Generated password.</returns>
+        public static string GeneratePassword(bool useLowercase, bool useUppercase,
+            bool useNumbers, bool useSpecial, int passwordSize)
+        {
+            char[] _password = new char[passwordSize];
+            string charSet = ""; // Initialise to blank
+            Random _random = new Random();
+            int counter;
+
+            // Build up the character set to choose from
+            if (useLowercase) charSet += LOWER_CASE;
+
+            if (useUppercase) charSet += UPPER_CASE;
+
+            if (useNumbers) charSet += NUMBERS;
+
+            if (useSpecial) charSet += SPECIALS;
+
+            for (counter = 0; counter < passwordSize; counter++)
+            {
+                _password[counter] = charSet[_random.Next(charSet.Length - 1)];
+            }
+
+            return String.Join(null, _password);
+        }
+    }
+}
+
