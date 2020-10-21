756a2f819887ac4e9cad6be32355fd9871c1a1c9
DevVeryMe <andrewveremiyy@gmail.com>
2020-10-21 17:55:30 +0300

    [JTCNB-76] Implemented EmailMessage builder methods.

---
+++ TripFlip/TripFlip.Services/Extensions/EmailMessageExtensions.cs
@@ -0,0 +1,33 @@
+﻿using SendGrid.Helpers.Mail;
+using TripFlip.Services.Interfaces.Models;
+
+namespace TripFlip.Services.Extensions
+{
+    public static class EmailMessageExtensions
+    {
+        public static void SetEmailFrom(this EmailMessage message, EmailAddress from)
+        {
+            message.From = from;
+        }
+
+        public static void SetEmailTo(this EmailMessage message, EmailAddress to)
+        {
+            message.To = to;
+        }
+
+        public static void SetEmailSubject(this EmailMessage message, string subject)
+        {
+            message.Subject = subject;
+        }
+
+        public static void SetEmailHtmlContent(this EmailMessage message, string htmlContent)
+        {
+            message.HtmlContent = htmlContent;
+        }
+
+        public static void SetEmailPlainTextContent(this EmailMessage message, string plainTextContent)
+        {
+            message.PlainTextContent = plainTextContent;
+        }
+    }
+}
---
+++ TripFlip/TripFlip.Services/Helpers/EmailMessageBuilder.cs
@@ -0,0 +1,27 @@
+﻿using SendGrid.Helpers.Mail;
+using TripFlip.Services.Interfaces.Models;
+
+namespace TripFlip.Services.Helpers
+{
+    public static class EmailMessageBuilder
+    {
+        public static EmailMessage Build(
+            EmailAddress from,
+            EmailAddress to,
+            string subject,
+            string htmlContent,
+            string plainTextContent)
+        {
+            var emailMessage = new EmailMessage()
+            {
+                From = from,
+                To = to,
+                Subject = subject,
+                HtmlContent = htmlContent,
+                PlainTextContent = plainTextContent
+            };
+
+            return emailMessage;
+        }
+    }
+}
