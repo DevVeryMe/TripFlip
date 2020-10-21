609a74c2183d4ee0d460d86394ad5be07203d400
DevVeryMe <andrewveremiyy@gmail.com>
2020-10-21 18:16:10 +0300

    [JTCNB-76] Added summary.

--- TripFlip/TripFlip.Services/Extensions/EmailMessageExtensions.cs
+++ TripFlip/TripFlip.Services/Extensions/EmailMessageExtensions.cs
@@ -3,28 +3,56 @@ using TripFlip.Services.Interfaces.Models;
 
 namespace TripFlip.Services.Extensions
 {
+    /// <summary>
+    /// EmailMessage class extension methods.
+    /// </summary>
     public static class EmailMessageExtensions
     {
+        /// <summary>
+        /// Sets EmailMessage From field with given value.
+        /// </summary>
+        /// <param name="message">EmailMessage instance.</param>
+        /// <param name="from">Email message sender.</param>
         public static void SetEmailFrom(this EmailMessage message, EmailAddress from)
         {
             message.From = from;
         }
 
+        /// <summary>
+        /// Sets EmailMessage To field with given value.
+        /// </summary>
+        /// <param name="message">EmailMessage instance.</param>
+        /// <param name="to">Email message receiver.</param>
         public static void SetEmailTo(this EmailMessage message, EmailAddress to)
         {
             message.To = to;
         }
 
+        /// <summary>
+        /// Sets EmailMessage Subject field with given value.
+        /// </summary>
+        /// <param name="message">EmailMessage instance.</param>
+        /// <param name="subject">Email message subject.</param>
         public static void SetEmailSubject(this EmailMessage message, string subject)
         {
             message.Subject = subject;
         }
 
+        /// <summary>
+        /// Sets EmailMessage HtmlContent field with given value.
+        /// </summary>
+        /// <param name="message">EmailMessage instance.</param>
+        /// <param name="htmlContent">Email message html content.</param>
         public static void SetEmailHtmlContent(this EmailMessage message, string htmlContent)
         {
             message.HtmlContent = htmlContent;
         }
 
+        /// <summary>
+        /// Sets EmailMessage PlainTextContent field with given value.
+        /// </summary>
+        /// <param name="message">EmailMessage instance.</param>
+        /// <param name="plainTextContent">Email message plain text content.</param>
         public static void SetEmailPlainTextContent(this EmailMessage message, string plainTextContent)
         {
             message.PlainTextContent = plainTextContent;
--- TripFlip/TripFlip.Services/Helpers/EmailMessageBuilder.cs
+++ TripFlip/TripFlip.Services/Helpers/EmailMessageBuilder.cs
@@ -3,8 +3,20 @@ using TripFlip.Services.Interfaces.Models;
 
 namespace TripFlip.Services.Helpers
 {
+    /// <summary>
+    /// Helper class for building EmailMessage.
+    /// </summary>
     public static class EmailMessageBuilder
     {
+        /// <summary>
+        /// Sets EmailMessage fields with given parameters' values.
+        /// </summary>
+        /// <param name="from">Email message sender.</param>
+        /// <param name="to">Email message receiver.</param>
+        /// <param name="subject">Email message subject.</param>
+        /// <param name="htmlContent">Email message html content.</param>
+        /// <param name="plainTextContent">Email message plain text content.</param>
+        /// <returns>An instance of EmailMessage.</returns>
         public static EmailMessage Build(
             EmailAddress from,
             EmailAddress to,
